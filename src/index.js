a3043e2df32e5ff2c7a6f39e62ecfb3dcaef073d
paulina232 <paulinamuar@gmail.com>
2023-05-18 10:38:36 -0500

    Commmit desde el computador de paulina

--- src/index.js
+++ src/index.js
@@ -58,3 +58,5 @@ app.use(express.static(path.join(__dirname,'public')));
 app.listen(app.get('port'),()=>{
     console.log(`Server on port ${app.get('port')}`);
 });
+
+//Esto es un comentario grupal para la clase de pablo
