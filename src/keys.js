fb879e689de23dc890aec7dda0484cdc2ad1a3e1
paulina232 <paulinamuar@gmail.com>
2023-05-30 10:52:16 -0500

    Actualizacion de keys de la base de datos

--- src/keys.js
+++ src/keys.js
@@ -2,9 +2,9 @@ module.exports={
     database:{
         database:"railway",
         user:"root",
-        host:"containers-us-west-154.railway.app",
-        password:"nHPYta2TzEuVIt1he8gI",
-        port:5913
+        host:"containers-us-west-157.railway.app",
+        password:"6JdQjTWyxxb1HL2BDLnq",
+        port:6220
         
     }
 };
